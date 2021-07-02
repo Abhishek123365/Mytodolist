0cb49d3c775b103787b1c1411dae84372077a761
Abhishek Singh <abhishek123singh365@gmail.com>
2021-07-02 14:29:08 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            We provide solutions of real life problem using infromation technology
+            <p>our primary goal is to provide solutions for the clients problems in best posiible way</p>
+        </div>
+    )
+}
